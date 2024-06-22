a1c9b819a238df99e240501b095fbbdca40b9a28
Orie Steele <orie@transmute.industries>
2024-06-22 10:06:04 -0500

    add party info

--- tests/jwt.test.ts
+++ tests/jwt.test.ts
@@ -51,3 +51,34 @@ it('Encrypted JWT with HPKE-Base-P256-SHA256-A128GCM', async () => {
   expect(result.protectedHeader.epk).toBeUndefined() 
   // encapsulated key is transported through "encrypted_key"
 })
+
+it('Encrypted JWT with ECDH-ES+A128KW and A128GCM, and party info', async () => {
+  const keys = await jose.generateKeyPair('ECDH-ES+A128KW', { extractable: true })
+  const jwt = await new jose.EncryptJWT(claims)
+  .setKeyManagementParameters({
+    "apu": jose.base64url.decode("QWxpY2U"),
+    "apv": jose.base64url.decode("Qm9i"),
+  })
+  .setProtectedHeader({ 
+    alg: 'ECDH-ES+A128KW', 
+    enc: 'A128GCM', 
+    apu: jose.base64url.encode("Initiator Public Identity"), 
+    apv: jose.base64url.encode("Responder Public Identity") })
+  .setIssuedAt()
+  .setIssuer('urn:example:issuer')
+  .setAudience('urn:example:audience')
+  .setExpirationTime('2h')
+  .encrypt(keys.privateKey)
+  // protected.encrypted_key.iv.ciphertext.tag
+  const result = await jose.jwtDecrypt(jwt, keys.privateKey)
+  expect(result.payload['urn:example:claim']).toBe(true)
+  expect(result.payload['iss']).toBe('urn:example:issuer')
+  expect(result.payload['aud']).toBe('urn:example:audience')
+  expect(result.payload.iat).toBeDefined()
+  expect(result.payload.exp).toBeDefined()
+  expect(result.protectedHeader['alg']).toBe('ECDH-ES+A128KW')
+  expect(result.protectedHeader['enc']).toBe('A128GCM')
+  expect(result.protectedHeader.epk).toBeDefined()
+  expect(result.protectedHeader.apu).toBe("QWxpY2U")
+  expect(result.protectedHeader.apv).toBe("Qm9i")
+})
